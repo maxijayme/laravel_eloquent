7fa5b97e8cdbca7c69a9f63986f48c9303ff0110
maxijayme <maxijayme@gmail.com>
2023-12-15 20:18:47 +0100

    first commit

---
+++ database/migrations/2023_12_15_073933_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->integer('city_id')->nullable();
+            $table->integer('company_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->string('name',30)->nullable();
+            $table->date('execution_date')->nullable();
+            $table->tinyInteger('is_active')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
